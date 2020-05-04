959280fcf9b81e365a8e0a8e4f6a31291228b952
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-04 17:03:43 -0400

    post users setup

--- index.js
+++ index.js
@@ -1,2 +1,41 @@
-const express = require("express"); 
+const express = require("express"); // CommonJS Modules
 
+const server = express();
+
+server.use(express.json()); // teaches express how to read JSON from the body
+
+let lessons = [
+  {
+    id: 1,
+    name: "Meow Meow",
+  },
+];
+
+server.get("/", (req, res) => {
+  res.json({ api: "Wow its working!" });
+});
+
+server.get("/api/users", function (req, res) {
+  // return an array of lessons (id, name)
+
+  res.json(lessons);
+});
+
+server.post("/api/users", function (req, res) {
+  const lessonInformation = req.body;
+
+  lessons.push(lessonInformation);
+
+  res.status(201).json(lessonInformation);
+});
+
+server.delete("/api/lessons/:id", function (req, res) {
+  const id = Number(req.params.id);
+
+  // find the lesson on the array and remove it
+  lessons = lessons.filter(lesson => lesson.id !== id);
+
+  res.status(200).json(lessons);
+});
+
+server.listen(8000, () => console.log("\n== API is up ==\n"));
