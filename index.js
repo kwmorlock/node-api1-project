b7f7c7c0424748b9f27b7d59abbe8ad2bf4b04cd
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-04 18:46:08 -0400

    delete and get working

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const express = require("express"); // CommonJS Modules
 
+const shortid = require('shortid');
+
 const server = express();
 
 server.use(express.json()); // teaches express how to read JSON from the body
 
 let kitties = [
   {
-    id: 1,
+    id: "1",
     name: "Kitty",
     bio: "Meow!"
   },
 ];
 
 server.get("/api/users/", (req, res) => {
-  res.json({ api: "Wow its working!" });
+
+  res.status(200).json(kitties)
 });
 
 server.get("/api/users/:id", function (req, res) {
@@ -26,8 +29,9 @@ server.post("/api/users", function (req, res) {
   const kittyInformation = req.body;
 
   if(kittyInformation.name && kittyInformation.bio){
-  kitties.push(kittyInformation);
-
+  kittyInformation.id = shortid.generate()
+  //shortid to generate a new id
+    kitties.push(kittyInformation);
   res.status(201).json(kittyInformation);
   } else {
       res.status(400).json({errorMessage: "Please provide name and bio for the user."})
@@ -35,12 +39,19 @@ server.post("/api/users", function (req, res) {
 });
 
 server.delete("/api/users/:id", function (req, res) {
-  const id = Number(req.params.id);
-
+const oldLength = kitties.length
   // find the lesson on the array and remove it
-  kitties = kitties.filter(kitty => kitty.id !== id);
+  kitties = kitties.filter(kitty => kitty.id !== req.params.id);
 
+  if(oldLength === kitties.length){
+    res.status(404).json({message: "The user with the specified ID does not exist."})
+  }
   res.status(200).json(kitties);
+//   res.status(200).json(kitties);
 });
+
+server.patch("/api/users/:id", function (req, res) {
+    const id = req.params.id;
+})
 
 server.listen(8000, () => console.log("\n== API is up ==\n"));
