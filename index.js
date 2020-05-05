f88cb687d5c1b448d2264bedf6840330523484df
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-05 16:47:56 -0400

    put updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express"); // CommonJS Modules
 
-const shortid = require('shortid');
+const shortid = require("shortid");
 
 const server = express();
 
@@ -10,13 +10,12 @@ let kitties = [
   {
     id: "1",
     name: "Kitty",
-    bio: "Meow!"
+    bio: "Meow!",
   },
 ];
 
 server.get("/api/users/", (req, res) => {
-
-  res.status(200).json(kitties)
+  res.status(200).json(kitties);
 });
 
 server.get("/api/users/:id", function (req, res) {
@@ -28,48 +27,52 @@ server.get("/api/users/:id", function (req, res) {
 server.post("/api/users", function (req, res) {
   const kittyInformation = req.body;
 
-  if(kittyInformation.name && kittyInformation.bio){
-  kittyInformation.id = shortid.generate()
-  //shortid to generate a new id
+  if (kittyInformation.name && kittyInformation.bio) {
+    kittyInformation.id = shortid.generate();
+    //shortid to generate a new id
     kitties.push(kittyInformation);
-  res.status(201).json(kittyInformation);
+    res.status(201).json(kittyInformation);
   } else {
-      res.status(400).json({errorMessage: "Please provide name and bio for the user."})
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
   }
 });
 
 server.delete("/api/users/:id", function (req, res) {
-const oldLength = kitties.length
+  const oldLength = kitties.length;
   // find the lesson on the array and remove it
-  kitties = kitties.filter(kitty => kitty.id !== req.params.id);
+  kitties = kitties.filter((kitty) => kitty.id !== req.params.id);
 
-  if(oldLength === kitties.length){
-    res.status(404).json({message: "The user with the specified ID does not exist."})
+  if (oldLength === kitties.length) {
+    res
+      .status(404)
+      .json({ message: "The user with the specified ID does not exist." });
   }
   res.status(500).json(kitties);
-//   res.status(500).json(kitties);
+  //   res.status(500).json(kitties);
 });
 
 server.put("/api/users/:id", function (req, res) {
-    const id = req.params.id;
-const kitty = kitties.find(kitty => kitty.id === id)
+  const id = req.params.id;
+  //creating a new const variable named id
+  const kitty = kitties.find((kitty) => kitty.id === id);
 
-if () {
-
-    res.status(404).json({message: "The user with the specified ID does not exist."})
-} else if(!req.body.name || !req.body.bio){
-
-    res.status(400).json({errorMessage: "Please provide name and bio for the user."})
-    
-} else if(){
-
-    res.status(500).json({errorMessage: "The user information could not be modified."})
-
-} else {
-
-    res.status(200).json(kitties);
-}
-
-})
+  if (!kitty) {
+    res
+      .status(404)
+      .json({ message: "The user with the specified ID does not exist." });
+  } else {
+    if (!req.body.name || !req.body.bio) {
+      res
+        .status(400)
+        .json({ errorMessage: "Please provide name and bio for the user." });
+    } else {
+      kitty.name=req.body.name
+      kitty.bio=req.body.bio
+      res.status(200).json(kitty);
+    }
+  }
+});
 
 server.listen(8000, () => console.log("\n== API is up ==\n"));
